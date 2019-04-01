b2365aca51f32870685ac31575b80e93b440ecd7
u211-06 <u211-06@public.mpei.local>
2019-04-01 10:28:11 +0300

    korrektnaya rabota programmi s funkciyami

--- main.cpp
+++ main.cpp
@@ -3,12 +3,9 @@
 
 using namespace std;
 
-int
-main() {
-    // Ввод данных
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
+vector <double> enter(int number_count)
+{
+
 
     cerr << "Enter numbers: ";
     vector<double> numbers(number_count);
@@ -16,13 +13,12 @@ main() {
         cin >> numbers[i];
     }
 
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
 
-    // Обработка данных
-    double min = numbers[0];
-    double max = numbers[0];
+    return numbers;
+}
+void  find_minmax(vector<double> numbers, double& min, double& max) {
+    min = numbers[0];
+    max = numbers[0];
     for (double number : numbers) {
         if (number < min) {
             min = number;
@@ -31,21 +27,32 @@ main() {
             max = number;
         }
     }
-
+}
+ vector<size_t> make_histogram(vector<double>numbers, size_t bin_count)
+{
+    size_t bin;
+    double min;
+    double max;
+    find_minmax(numbers, min, max);
     vector<size_t> bins(bin_count);
-    for (double number : numbers) {
-        size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
+    for (double number : numbers)
+    {
+        bin=0;
+        bin = (size_t)((number - min) / (max - min) * bin_count);
+        if (bin == bin_count)
+        {
             bin--;
         }
         bins[bin]++;
     }
-
-    // Вывод данных
+    return bins;
+}
+void show_gistogram(vector<size_t> bins)
+{   size_t bin;
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
-    size_t max_count = 0;
+   size_t max_count = 0;
     for (size_t count : bins) {
         if (count > max_count) {
             max_count = count;
@@ -74,5 +81,84 @@ main() {
         cout << '\n';
     }
 
+
+}
+
+
+
+int main() {
+    // Ввод данных
+
+
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+   const vector<double> numbers = enter(number_count);
+
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+
+
+
+
+   double min, max;
+    find_minmax(numbers, min, max);
+
+    // Обработка данных
+    //double min = numbers[0];
+    //double max = numbers[0];
+    //for (double number : numbers) {
+        //if (number < min) {
+        //    min = number;
+        //}
+        //if (number > max) {
+        //    max = number;
+       // }
+  //  }
+
+    //vector<size_t> bins(bin_count);
+    //for (double number : numbers) {
+       // size_t bin = (size_t)((number - min) / (max - min) * bin_count);
+       // if (bin == bin_count) {
+       //     bin--;
+       // }
+        //bins[bin]++;
+   // }
+    vector<size_t> bins = make_histogram(numbers, bin_count);
+    // Вывод данных
+    //const size_t SCREEN_WIDTH = 80;
+    //const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
+
+    //size_t max_count = 0;
+    //for (size_t count : bins) {
+      //  if (count > max_count) {
+        //    max_count = count;
+        //}
+    //}
+    //const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    //for (size_t bin : bins) {
+      //  if (bin < 100) {
+        //    cout << ' ';
+        //}
+        //if (bin < 10) {
+          //  cout << ' ';
+        //}
+        //cout << bin << "|";
+
+        //size_t height = bin;
+        //if (scaling_needed) {
+          //  const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            //height = (size_t)(bin * scaling_factor);
+        //}
+
+        //for (size_t i = 0; i < height; i++) {
+          //  cout << '*';
+        //}
+        //cout << '\n';
+    //}
+show_gistogram(bins);
     return 0;
 }
