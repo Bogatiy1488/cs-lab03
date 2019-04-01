9e826e3d85dc483e8929352970c7af216160a8d5
u211-06 <u211-06@public.mpei.local>
2019-04-01 12:29:12 +0300

    last commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include "histogram.h"
 using namespace std;
 
 vector <double> enter(int number_count)
@@ -16,18 +16,7 @@ vector <double> enter(int number_count)
 
     return numbers;
 }
-void  find_minmax(vector<double> numbers, double& min, double& max) {
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
-            min = number;
-        }
-        if (number > max) {
-            max = number;
-        }
-    }
-}
+
  vector<size_t> make_histogram(vector<double>numbers, size_t bin_count)
 {
     size_t bin;
